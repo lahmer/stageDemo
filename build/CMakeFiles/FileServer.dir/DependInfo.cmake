f10d15ae34500d0e3a055243439983e84b4d87e5
lahmer <lahmer.ilyas@hotmail.com>
2017-01-27 23:14:30 +0100

    final

---
+++ build/CMakeFiles/FileServer.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/lahmer/Desktop/stageDemo/File.cpp" "/home/lahmer/Desktop/stageDemo/build/CMakeFiles/FileServer.dir/File.cpp.o"
+  "/home/lahmer/Desktop/stageDemo/FileServer.cpp" "/home/lahmer/Desktop/stageDemo/build/CMakeFiles/FileServer.dir/FileServer.cpp.o"
+  "/home/lahmer/Desktop/stageDemo/Server.cpp" "/home/lahmer/Desktop/stageDemo/build/CMakeFiles/FileServer.dir/Server.cpp.o"
+  "/home/lahmer/Desktop/stageDemo/main.cpp" "/home/lahmer/Desktop/stageDemo/build/CMakeFiles/FileServer.dir/main.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ build/CMakeFiles/FileServer.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/FileServer.dir/main.cpp.o"
+  "CMakeFiles/FileServer.dir/FileServer.cpp.o"
+  "CMakeFiles/FileServer.dir/File.cpp.o"
+  "CMakeFiles/FileServer.dir/Server.cpp.o"
+  "FileServer.pdb"
+  "FileServer"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/FileServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
